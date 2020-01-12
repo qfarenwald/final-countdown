b5d02be7416af931d9fd4a836efad4b99e3ef319
Quinne Farenwald <q.farenwald@gmail.com>
2020-01-12 16:23:30 -0700

    Create clean App.vue template

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,28 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Find Images Beyond Your Wildest Dreams"/>
-    <input v-model="image" placeholder="ENTER DREAM HERE">
-    <button>SEARCH</button>
-    <ul>
-      <li>insert image components here</li>
-    </ul>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
 </style>
